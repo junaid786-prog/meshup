34ecda52660cce70382b7b4bcc7cc4deb59ef980
junaid786-prog <junaidshoaib555@gmail.com>
2025-02-14 15:27:59 +0500

    fix: auth middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
-router.get("/me", authController.getProfile);
+router.get("/me", authMiddleware, authController.getProfile);
 
 module.exports = router;
